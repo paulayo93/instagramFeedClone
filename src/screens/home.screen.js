baf6fba1da65b8338e8e28cff2d0086db26d9b3a
Paul Oloyede <52790286+paulayo93@users.noreply.github.com>
2022-10-16 00:39:30 +0100

    rendering content

--- src/screens/home.screen.js
+++ src/screens/home.screen.js
@@ -4,8 +4,8 @@ import {
   Text,
   View,
   TouchableWithoutFeedback,
+  Image,
   ImageBackground,
-  Image
 } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { productApi } from "./../redux-store/product.effects";
@@ -16,12 +16,84 @@ import Explore from "./../assets/icons/explore";
 import Like from "./../assets/icons/like";
 import Messenger from "./../assets/icons/spike";
 import story from "./../assets/images/stories.png";
-import color from './../constants/colors'
+import color from "./../constants/colors";
 
-const Header = (props) => {
+import dog from "./../assets/images/dog.png";
+import Option from "./../assets/icons/option";
+
+const Header = ({ image, title, status }) => {
   return (
     <View>
-      <Image source={story} style={{ width: 375, height: 105}} />
+      <Image source={story} style={{ width: 375, height: 105 }} />
+    </View>
+  );
+};
+
+const Avatar = (props) => {
+  return (
+    <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <Image
+        style={{
+          width: 30,
+          height: 30,
+          borderRadius: 30,
+          borderWidth: 0,
+          borderColor: "none",
+          marginRight: 9,
+        }}
+        source={dog}
+      />
+      <Text
+        style={{
+          fontSize: 12,
+          lineHeight: 22,
+          fontWeight: "bold",
+          color: "#000000",
+        }}
+      >
+        Ruffles
+      </Text>
+    </View>
+  );
+};
+
+const ContentHeader = () => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingHorizontal: 8,
+        paddingVertical: 10,
+        marginBottom: 3
+      }}
+    >
+      <Avatar />
+      <Option />
+    </View>
+  );
+};
+
+const Content = ({ image, video, type, title }) => {
+  return (
+    <View style={{flex: 1, height: 400}}>
+      <Image
+      style={{
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        position: "absolute",
+        // aspectRatio: 1
+      }}
+        source={{
+          uri: image,
+        }}
+
+        resizeMode="contain"
+      />
+
     </View>
   );
 };
@@ -31,10 +103,15 @@ export default function HomeScreen() {
   const { data, error, isLoading } =
     productApi.endpoints.getAllProducts.useQuery();
 
-  const getProductsStore = useSelector((state) => state.product);
+  // const getProductsStore = useSelector((state) => state.product);
 
   return (
-    <AppContainer light={false} backgroundColor={color.backgroundColor} padded={false} scroll={false}>
+    <AppContainer
+      light={false}
+      backgroundColor={color.backgroundColor}
+      padded={false}
+      scroll={false}
+    >
       <View
         style={{
           marginTop: 18,
@@ -55,34 +132,39 @@ export default function HomeScreen() {
             height: 24,
             flexDirection: "row",
             justifyContent: "space-between",
-            paddingRight: 5
+            paddingRight: 5,
           }}
         >
           <TouchableWithoutFeedback onPress={() => null}>
-          <Explore />
+            <Explore />
           </TouchableWithoutFeedback>
 
           <TouchableWithoutFeedback onPress={() => null}>
-          <Like />
+            <Like />
           </TouchableWithoutFeedback>
 
-          <TouchableWithoutFeedback onPress={() => console.log('Yeah')}>
-          <Messenger />
+          <TouchableWithoutFeedback onPress={() => null}>
+            <Messenger />
           </TouchableWithoutFeedback>
-        
-       
-         
         </View>
       </View>
+
       <FlashList
         ListHeaderComponent={<Header />}
         data={data}
         estimatedItemSize={200}
         renderItem={({ item }) => {
+          // console.log(item.image)
           return (
-            <View style={styles.container}>
-              <Text style={{ color: "black" }}>Here</Text>
-            </View>
+            <>
+              <View style={{marginBottom: 40}}>
+
+            
+                <ContentHeader />
+                <Content image={item.image} />
+                </View>
+           
+            </>
           );
         }}
       />
